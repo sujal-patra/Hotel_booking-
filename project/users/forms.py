084d9a779dc70d18dfb1eec63f16c097aeceadfe
sujal-patra <25sujalpatra@gmail.com>
2024-07-24 15:20:06 +0530

    Project
    Hotel booking

---
+++ project/users/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import UserModel
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = ['first_name', 'last_name', 'username', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100, required=True)
+    password = forms.CharField(widget=forms.PasswordInput,required=True)
