4667e6a8af01ddde3f5b0c157e459a0d2128ff9a
sujal-patra <25sujalpatra@gmail.com>
2024-07-27 21:23:18 +0530

    Delete project directory

--- project/hotels/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Hotel,Room,Booking
-
-# Register your models here.
-admin.site.register(Hotel)
-admin.site.register(Room)
-admin.site.register(Booking)
